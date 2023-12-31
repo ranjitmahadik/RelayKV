c8af673e1bd6607422414e515fe4b2140cc9b523
CW-RanjitMahadik <ranjit@carwale.com>
2023-12-31 17:27:31 +0530

    - load env variables and set them.

--- src/main/java/com/dkv/distributedkvstore/DistributedKvStoreApplication.java
+++ src/main/java/com/dkv/distributedkvstore/DistributedKvStoreApplication.java
@@ -1,13 +1,23 @@
 package com.dkv.distributedkvstore;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DistributedKvStoreApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DistributedKvStoreApplication.class, args);
-	}
+    public static void loadAndSetEnvVariables() {
+        Dotenv dotenv = Dotenv.load();
+        String postgresUsername = dotenv.get("POSTGRES_USER");
+        String postgresPassword = dotenv.get("POSTGRES_PASSWORD");
+        System.setProperty("POSTGRES_USER", postgresUsername);
+        System.setProperty("POSTGRES_PASSWORD", postgresPassword);
+    }
+
+    public static void main(String[] args) {
+        loadAndSetEnvVariables();
+        SpringApplication.run(DistributedKvStoreApplication.class, args);
+    }
 
 }
